3c06f8fe7819b4d675f8cbcfae27bf0f0e5b8e2c
felipetaua <taua.felipee@gmail.com>
2025-01-07 21:35:24 -0300

    iniciando js barra nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,28 @@ window.addEventListener("load", function () {
     this.document.body.classList.remove("active");
 });
 
+// Mobile nav toggle
+
+const [navTogglers, navLinks, navbar, overley] = [
+    document.querySelectorAll("[data-nav-toggler]"),
+    document.querySelectorAll("[data-nav-link]"),
+    document.querySelector("[data-navbar]"),
+    document.querySelector("[data-overlay]")
+];
+
+const toggleNav = function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNav);
+
+const closeNav = function () {
+    navbar.classList.remove("active");
+    overlay.classList.remove("active");
+    document.body.classList.remove("active");
+}
+
+addEventOnElements(navLinks, "click", closeNav);
+
