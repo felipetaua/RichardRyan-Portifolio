e75623f9165c57d28fb11915439890a24f4f76cb
felipetaua <taua.felipee@gmail.com>
2025-01-07 21:43:41 -0300

    finalizado barra de navegação

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,3 +41,17 @@ const closeNav = function () {
 
 addEventOnElements(navLinks, "click", closeNav);
 
+
+// Header
+
+const header = document.querySelector("[data-header]");
+
+const activeElementOnScroll = function () {
+    if (window.scrollY > 50) {
+        header.classList.add("active");
+    } else {
+        header.classList.remove("active");
+    }
+}
+
+window.addEventListener("scroll", activeElementOnScroll);
