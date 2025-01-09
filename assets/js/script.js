778d4fb0ed7b60f3fc371121e6d632e27e48fe97
felipetaua <taua.felipee@gmail.com>
2025-01-09 11:32:03 -0300

    finalizando js do efeito de animação texto

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,51 +58,82 @@ window.addEventListener("scroll", activeElementOnScroll);
 
 
 /* text animation efeito hero section*/
+rBoxes = document.querySelectorAll("[data-letter-effect]");
 
-const letterBoxes = document.querySelectorAll("[data-letter-effect]");
-
-let activeLetterBoxesIndex = 0;
+let activeLetterBoxIndex = 0;
 let lastActiveLetterBoxIndex = 0;
 let totalLetterBoxDelay = 0;
 
 const setLetterEffect = function () {
-    // loop throush all letter boxes
-    for (let i = 0; i < letterBoxes.length; i++) {
-        // set initial animation delay
-        let letterAnimationDelay = 0;
 
-        // get all character from the current letter box
-        const letters = letterBoxes[i].textContent.trim();
-        // remove all character from the current letter box
-        letterBoxes[i].textContent = "";
+  // loop through all letter boxes
+  for (let i = 0; i < letterBoxes.length; i++) {
+    // set initial animation delay
+    let letterAnimationDelay = 0;
 
-        // loop through all letters
-        for (let j = 0; j < letters.length; j++) {
+    // get all character from the current letter box
+    const letters = letterBoxes[i].textContent.trim();
+    // remove all character from the current letter box
+    letterBoxes[i].textContent = "";
 
-            // create span
-            const span = document.createElement("span");
+    // loop through all letters
+    for (let j = 0; j < letters.length; j++) {
 
-            // set animation delay on span
-            span.style.animationDelay = `${letterAnimationDelay}`;
+      // create a span
+      const span = document.createElement("span");
 
-            // set th "in" class on the span, if current letter box is active
-            // otherwise class is "out"
-            if (i === activeLetterBoxesIndex) {
-                span.classList.add("in");
-            } else {
-                span.classList.add("out");
-            }
+      // set animation delay on span
+      span.style.animationDelay = `${letterAnimationDelay}s`;
 
-            // pass current letter into span
-            span.textContent = letters[j];
+      // set the "in" class on the span, if current letter box is active
+      // otherwise class is "out"
+      if (i === activeLetterBoxIndex) {
+        span.classList.add("in");
+      } else {
+        span.classList.add("out");
+      }
 
-            //add space class on span, when current letter contain space
-            if (letters[j] === "") span.classList.add("space");
+      // pass current letter into span
+      span.textContent = letters[j];
 
-            // pass the span on current letter box
-            letterBoxes[i].appendChild(span);
+      // add space class on span, when current letter contain space
+      if (letters[j] === " ") span.classList.add("space");
 
-            // skip letterAnimationDelay when loop is in the last index
-        }
+      // pass the span on current letter box
+      letterBoxes[i].appendChild(span);
+
+      // skip letterAnimationDelay when loop is in the last index
+      if (j >= letters.length - 1) break;
+      // otherwise update
+      letterAnimationDelay += 0.05;
+
     }
+
+    // get total delay of active letter box
+    if (i === activeLetterBoxIndex) {
+      totalLetterBoxDelay = Number(letterAnimationDelay.toFixed(2));
+    }
+
+    // add active class on last active letter box
+    if (i === lastActiveLetterBoxIndex) {
+      letterBoxes[i].classList.add("active");
+    } else {
+      letterBoxes[i].classList.remove("active");
+    }
+
+  }
+
+  setTimeout(function () {
+    lastActiveLetterBoxIndex = activeLetterBoxIndex;
+
+    // update activeLetterBoxIndex based on total letter boxes
+    activeLetterBoxIndex >= letterBoxes.length - 1 ? activeLetterBoxIndex = 0 : activeLetterBoxIndex++;
+
+    setLetterEffect();
+  }, (totalLetterBoxDelay * 1000) + 3000);
+
 }
+
+// call the letter effect function after window loaded
+window.addEventListener("load", setLetterEffect);
+
